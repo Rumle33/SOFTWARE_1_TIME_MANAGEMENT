6ac4ed7d63853be814cea9d0e6ad286bffe3aecb
rumlekjaerulso <rumlekjaerulso@hotmail.com>
2022-04-24 13:46:00 +0200

    FX

--- src/main/java/dtu/GUIApplication.java
+++ src/main/java/dtu/GUIApplication.java
@@ -1,13 +1,14 @@
 package dtu;
 
-import javafx.fxml.Initializable;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class GUIApplication implements Initializable {
+public class GUIApplication extends Application {
+
+
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void start(Stage stage) throws Exception {
 
     }
 }
