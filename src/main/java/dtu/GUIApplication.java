bbb0b261e6a31399c986381b95d26604b859797c
Andersraae <andersraae01@gmail.com>
2022-04-25 15:14:49 +0200

    Yes 2

--- src/main/java/dtu/GUIApplication.java
+++ src/main/java/dtu/GUIApplication.java
@@ -1,14 +1,14 @@
 package dtu;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+
 
 public class GUIApplication implements ActionListener {
 
-    private JPanel panel1;
-    private JButton casperButton;
-    private JButton jensButton;
+    public JPanel panel1;
+    public JButton casperButton;
+    public JButton jensButton;
     JFrame frame = new JFrame("Time management application");
 
     GUIApplication() {
--- src/main/java/dtu/Main.java
+++ src/main/java/dtu/Main.java
@@ -1,6 +1,6 @@
 package dtu;
 
-public class mainPage {
+public class Main{
     public static void main(String[] args) {
         GUIApplication GuiApplication = new GUIApplication();
 
